d10b1c009ba87125f77312602497b58cfb293709
SergTaranenko <rates-13@inbox.ru>
2021-02-15 12:52:48 +0300

    1

--- src/main/java/Ho11/ArrayUtils.java
+++ src/main/java/Ho11/ArrayUtils.java
@@ -1,4 +1,21 @@
 package Ho11;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ArrayUtils {
+
+    public static <T> void replaceTwoElements(T[] arr, int i1, int i2) {
+        T obj = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = obj;
+    }
+
+    public static <T> ArrayList<T> arrToList(T[] arr) {
+        ArrayList<T> result = new ArrayList<>(arr.length);
+        for (T t : arr) {
+            result.add(t);
+        }
+        return result;
+    }
 }
--- src/main/java/Ho11/Box.java
+++ src/main/java/Ho11/Box.java
@@ -1,4 +1,44 @@
 package Ho11;
 
-public class Box {
+        import java.util.Arrays;
+        import java.util.Collection;
+        import java.util.LinkedList;
+
+public class Box<T extends Fruit> {
+
+    LinkedList<T> fruits;
+
+    public Box() {
+        this.fruits = new LinkedList<>();
+    }
+
+    public Box(Collection<T> fruits) {
+        this.fruits = new LinkedList<>(fruits);
+    }
+
+    public Box(T... fruits) {
+        this.fruits = new LinkedList<>(Arrays.asList(fruits));
+    }
+
+    public void add(T fruit) {
+        fruits.add(fruit);
+    }
+
+    public void replaceAll(Box<T> other) {
+        other.getFruits().addAll(fruits);
+        fruits.clear();
+    }
+
+    public double getWeight() {
+        if (fruits.size() == 0) return 0;
+        return fruits.size() * fruits.get(0).getWEIGHT();
+    }
+
+    public boolean compare(Box<?> other) {
+        return Math.abs(getWeight() - other.getWeight()) < 0.001;
+    }
+
+    public LinkedList<T> getFruits() {
+        return fruits;
+    }
 }
